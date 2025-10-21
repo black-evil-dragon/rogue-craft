04a3198536b5cd3c8b7b9c2f58249a490624bf3a
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2025-10-21 19:07:31 +0300

    [sync] Синхронизация между устройствами

---
+++ src/client/java/code/purrfect/RoguecraftClient.java
@@ -0,0 +1,10 @@
+package code.purrfect;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoguecraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/code/purrfect/RoguecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package code.purrfect;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RoguecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
